ff0b7c2aab8077b0e0c077bc0baef70ab7492eea
Dikson Samuel <dikson@skcript.com>
2017-03-09 15:50:20 +0530

    Added volume control

--- RNStreamingAudioPlayer.h
+++ RNStreamingAudioPlayer.h
@@ -1,12 +1,12 @@
 //React Native Audio Player logic(no UI)
 
-#import "RCTBridgeModule.h"
-#import "RCTEventEmitter.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
 @import AVFoundation;
 
-@interface RNAudioPlayerURL : RCTEventEmitter <RCTBridgeModule>
+@interface RNStreamingAudioPlayer : RCTEventEmitter <RCTBridgeModule>
 
 @property (strong, nonatomic) AVPlayerItem *audioItem;
 @property (strong, nonatomic) AVPlayer *audioPlayer;
-
 @end
--- RNStreamingAudioPlayer.m
+++ RNStreamingAudioPlayer.m
@@ -1,9 +1,9 @@
 //React Native Audio Player logic(no UI)
 
-#import "RNAudioPlayerURL.h"
-#import "RCTBridge.h"
+#import "RNStreamingAudioPlayer.h"
+#import <React/RCTBridge.h>
 
-@implementation RNAudioPlayerURL
+@implementation RNStreamingAudioPlayer
 
 @synthesize bridge = _bridge;
 
@@ -11,14 +11,21 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(initWithURL:(NSString *)url){
   if(!([url length]>0)) return;
+    
   NSURL *soundUrl = [[NSURL alloc] initWithString:url];
+  
   self.audioItem = [AVPlayerItem playerItemWithURL:soundUrl];
+  
   self.audioPlayer = [AVPlayer playerWithPlayerItem:self.audioItem];
+  
   [[NSNotificationCenter defaultCenter]
     addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.audioItem];
+  
   [[NSNotificationCenter defaultCenter]
     addObserver:self selector:@selector(playerItemStalled:) name:AVPlayerItemPlaybackStalledNotification object:self.audioItem];
+  
 }
+
 
 - (void)playerItemDidReachEnd:(NSNotification *)notification{
   [self.audioItem seekToTime:kCMTimeZero];
@@ -28,17 +35,22 @@ RCT_EXPORT_METHOD(initWithURL:(NSString *)url){
   [self.audioPlayer play];
 }
 
+
+
 RCT_EXPORT_METHOD(getDuration:(RCTResponseSenderBlock)callback){
   while(self.audioItem.status != AVPlayerItemStatusReadyToPlay){
   }  //this is kind of crude but it will prevent the app from crashing due to a "NAN" return(this allows the getDuration method to be executed in the componentDidMount function of the React class without the app crashing
   float duration = CMTimeGetSeconds(self.audioItem.duration);
   callback(@[[[NSNumber alloc] initWithFloat:duration]]);
 }
+
+
 - (NSArray<NSString *> *)supportedEvents
 {
   NSArray *events = @[@"AudioEnded"];
   return events;
 }
+
 
 RCT_EXPORT_METHOD(play){
   [self.audioPlayer play];
@@ -46,10 +58,15 @@ RCT_EXPORT_METHOD(play){
 
 RCT_EXPORT_METHOD(pause){
   [self.audioPlayer pause];
+
 }
 
 RCT_EXPORT_METHOD(seekToTime:(nonnull NSNumber *)toTime){
   [self.audioPlayer seekToTime: CMTimeMakeWithSeconds([toTime floatValue], NSEC_PER_SEC)];
+}
+
+RCT_EXPORT_METHOD(setVolume:(nonnull NSNumber *)volumeValue){
+  self.audioPlayer.volume = [volumeValue floatValue];
 }
 
 @end
